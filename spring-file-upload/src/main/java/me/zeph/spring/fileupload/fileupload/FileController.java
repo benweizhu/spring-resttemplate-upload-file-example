31eb2a5412b93ea7c69fd649f0678fd048da09db
benweizhu <benweizhu@gmail.com>
2020-08-18 16:05:52 +0800

    init

---
+++ spring-file-upload/src/main/java/me/zeph/spring/fileupload/fileupload/FileController.java
@@ -0,0 +1,19 @@
+package me.zeph.spring.fileupload.fileupload;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@RestController
+public class FileController {
+
+  @CrossOrigin(origins = "*")
+  @PostMapping(value = "/upload")
+  public String uploadCSV(@RequestParam(value = "file") MultipartFile file) {
+    return file.getName();
+  }
+
+}
