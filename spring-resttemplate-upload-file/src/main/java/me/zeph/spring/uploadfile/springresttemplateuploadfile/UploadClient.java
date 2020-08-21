4588b9a6ee6b7899c5198cd9ecaf4fd8737cf409
benweizhu <benweizhu@gmail.com>
2020-08-21 16:18:26 +0800

    change part to param for date

--- spring-resttemplate-upload-file/src/main/java/me/zeph/spring/uploadfile/springresttemplateuploadfile/UploadClient.java
+++ spring-resttemplate-upload-file/src/main/java/me/zeph/spring/uploadfile/springresttemplateuploadfile/UploadClient.java
@@ -2,11 +2,13 @@ package me.zeph.spring.uploadfile.springresttemplateuploadfile;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 @FeignClient(value = "upload", url = "http://localhost:9091")
 public interface UploadClient {
   @PostMapping(value = "upload", consumes = "multipart/form-data")
-  String uploadCoupons(@RequestPart("file") MultipartFile file, @RequestPart("date") String date);
+  String uploadCoupons(@RequestPart("file") MultipartFile file,
+                       @RequestParam("date") String date);
 }
